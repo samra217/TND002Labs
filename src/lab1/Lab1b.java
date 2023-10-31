9da7d50d87c71b2a9372d4b6acc358b88a0e6502
samra217 <143489967+samra217@users.noreply.github.com>
2023-10-31 23:58:53 +0100

    s

--- src/lab1/Lab1b.java
+++ src/lab1/Lab1b.java
@@ -3,7 +3,6 @@ package lab1;
 public class Lab1b {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Vector v1 = new Vector(1,2,3);
 		Vector v2 = new Vector();
 	}
