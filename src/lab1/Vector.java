f3af1215b3baf861fd0fff6bcbc00e4b85976c75
samra217 <samra217@student.liu.se>
2023-11-07 12:18:58 +0100

    s

--- src/lab1/Vector.java
+++ src/lab1/Vector.java
@@ -4,27 +4,28 @@ public class Vector {
 	private double x = 0.0;
 	private double y = 0.0;
 	private double z = 0.0;
-	public double[] vdef = new double[3];
+	public double[] vdef = {x,y,z};
 	
 	public Vector() {
 		setToDefault();
 	}
 	
 	public Vector(double d1, double d2, double d3) {
-		this.vdef[0] = d1;
-		this.vdef[1] = d2;
-		this.vdef[2] = d3;
+		x = d1;
+		y = d2;
+		z = d3;
 	}
 	
 	public void setToDefault(double arg) {
-		this.vdef[0] = arg;
-		this.vdef[1] = arg;
-		this.vdef[2] = arg;
+		x = arg;
+		y = arg;
+		z = arg;
+		
 	}
 	
 	public void setToDefault() {
-		this.vdef[0] = x;
-		this.vdef[1] = y;
-		this.vdef[2] = z;
+		x = 0.0;
+		y = 0.0;
+		z = 0.0;
 	}
 }
