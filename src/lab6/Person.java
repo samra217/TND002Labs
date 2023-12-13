edb9c28090122f4e4eed847cb1b14c5addda97e6
samra217 <143489967+samra217@users.noreply.github.com>
2023-12-13 14:38:08 +0100

    person fixed

--- src/lab6/Person.java
+++ src/lab6/Person.java
@@ -1,5 +1,22 @@
 package lab6;
 
 public class Person {
-
+	private String givenName;
+	private String surName;
+	private int phoneNumber;
+	
+	public Person(String firstName, String lastName, int phone) {
+		this.givenName = firstName;
+		this.surName = lastName;
+		this.phoneNumber = phone;
+	}
+	
+	
+	public String getSurName() {
+		return this.surName;
+	}
+	
+	public Stringe getFullName() {
+		return this.givenName+" "+this.surName;
+	}
 }
