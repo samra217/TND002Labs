7ededba3bd3b25c880969bdba8e0a94f8e99dc49
samra217 <143489967+samra217@users.noreply.github.com>
2023-12-13 13:59:28 +0100

    added lab6

---
+++ src/lab6/GUI.java
@@ -0,0 +1,14 @@
+package lab6;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
